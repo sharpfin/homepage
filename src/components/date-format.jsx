e629058638749ffd9b972e1619d667e2c39bb903
Blixthalka <emil837@gmail.com>
2022-03-08 15:41:29 +0100

    safari date bug

--- src/components/date-format.jsx
+++ src/components/date-format.jsx
@@ -3,7 +3,7 @@ import { LangContext } from './context'
 
 export default function DateFormat({date, className}) {
     const { t } = React.useContext(LangContext)
-    const d = new Date(date)
+    const d = new Date(date.trim().replaceAll(" ", "-"))
     return(
         <p className={`text-gray-500 ${className}`}>{d.getDate() + ' ' + t.month(d.getMonth()) + ' ' + d.getFullYear()}</p>
     );
