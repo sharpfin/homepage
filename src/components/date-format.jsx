dc9c87e7057931e40def30ac5fab28bfa6446069
Blixthalka <emil837@gmail.com>
2022-03-28 13:16:23 +0200

    date

--- src/components/date-format.jsx
+++ src/components/date-format.jsx
@@ -3,7 +3,7 @@ import { LangContext } from './context'
 
 export default function DateFormat({date, className}) {
     const { t } = React.useContext(LangContext)
-    const d = new Date(date.trim().replaceAll(" ", "-"))
+    const d = new Date(date)
     return(
         <p className={`text-gray-500 ${className}`}>{d.getDate() + ' ' + t.month(d.getMonth()) + ' ' + d.getFullYear()}</p>
     );
