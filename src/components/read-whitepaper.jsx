6cabbd3105438f88acfdca858b45a09a0e72f222
Blixthalka <emil837@gmail.com>
2022-11-11 16:09:34 +0100

    bigger image

--- src/components/read-whitepaper.jsx
+++ src/components/read-whitepaper.jsx
@@ -27,7 +27,7 @@ const ReadWhitepaper = ({ data, langKey }) => {
                             </LangLink>
                         </div>
                     </div>
-                    <Img fluid={data.whitepaper.childImageSharp.fluid} className="w-72 rounded shadow-xl place-self-center order-1 md:order-2" />
+                    <Img fluid={data.whitepaper.childImageSharp.fluid} className="w-80 rounded shadow-xl place-self-center order-1 md:order-2" />
                 </div>
             </LayoutContainer>
         </div>
