badf19260c581e21017f42947ebf59596d5eedf8
Blixthalka <emil837@gmail.com>
2023-01-20 10:53:36 +0100

    estea -> inverde

--- src/components/trusted.jsx
+++ src/components/trusted.jsx
@@ -1,7 +1,7 @@
 import { graphql, StaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
 import * as React from "react";
-import Estea from '../images/companies/estea_white.svg';
+import Inverde from '../images/companies/inverde.svg';
 import LayoutContainer from './layout-container';
 import {LangContext} from "./context"
 
@@ -15,10 +15,10 @@ const Trusted = ({ data }) => {
             bgColorClass={"bg-sharpfin-blue"}
         >
             <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10 items-center justify-items-center lg:justify-items-start">
-                <Estea className="h-12" />
                 <Img fluid={data.nordnet.childImageSharp.fluid} className="w-44 " />
                 <Img fluid={data.consensus.childImageSharp.fluid} className="w-52" />
                 <Img fluid={data.safe.childImageSharp.fluid} className="w-52" />
+                <Inverde className="h-16 w-52" />
             </div>
         </LayoutContainer>
     )
