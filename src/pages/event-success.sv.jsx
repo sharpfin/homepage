78ec1a72f05b6fff5ad7b5168f9579a7fcfa3b66
Blixthalka <emil837@gmail.com>
2023-05-16 15:10:41 +0200

    remove fondo event

--- src/pages/event-success.sv.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import EventSuccessPage from '../components/page/event-success'
-
-const EventSuccess = (props) => {
-  const langKey = props.pageContext.langKey;
-  return (<EventSuccessPage langKey={langKey} />)
-}
-
-export default EventSuccess
--- src/pages/event.en.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import EventPage from '../components/page/event'
-
-const Event = (props) => {
-  const langKey = props.pageContext.langKey;
-  return (<EventPage langKey={langKey} />)
-}
-
-export default Event
